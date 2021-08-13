3bca9223ce63995531534df2c5c8062c1ab2c143
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-08-13 21:50:25 +0200

    Add usb hid device

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,22 @@ use core::sync::atomic::{AtomicUsize, Ordering};
 
 use adafruit_neotrellis::{self as neotrellis, neopixel, NeoPixels, NeoTrellis};
 use defmt_rtt as _;
-use embedded_hal::blocking::i2c::{Read, Write};
-use nrf52840_hal as _;
+use embedded_hal::{
+    blocking::i2c::{Read, Write},
+    timer::CountDown,
+};
+use nrf52840_hal::{
+    self as _,
+    usbd::{UsbPeripheral, Usbd},
+};
 use panic_probe as _;
+use usb_device::{
+    bus::UsbBusAllocator,
+    device::{UsbDeviceBuilder, UsbVidPid},
+};
+use usbd_hid::descriptor::generator_prelude::*;
+use usbd_hid::descriptor::MouseReport;
+use usbd_hid::hid_class::HIDClass;
 
 #[defmt::panic_handler]
 fn panic() -> ! {
@@ -81,14 +94,18 @@ impl<'a, I2C: Read + Write> BreathingLights<'a, I2C> {
     fn calculate_next_state(&mut self) {
         match self.direction {
             BreathingDirection::Increasing => {
-                self.value += 1;
-                if self.value == 255 {
+                if self.value < 245 {
+                    self.value += 10;
+                } else {
+                    self.value = 255;
                     self.direction = BreathingDirection::Decreasing;
                 }
             }
             BreathingDirection::Decreasing => {
-                self.value -= 1;
-                if self.value == 0 {
+                if self.value > 10 {
+                    self.value -= 10;
+                } else {
+                    self.value = 0;
                     self.direction = BreathingDirection::Increasing;
                 }
             }
@@ -110,6 +127,8 @@ impl<'a, I2C: Read + Write> BreathingLights<'a, I2C> {
 #[cortex_m_rt::entry]
 fn main() -> ! {
     let peripherals = nrf52840_hal::pac::Peripherals::take().unwrap();
+    let clocks = nrf52840_hal::clocks::Clocks::new(peripherals.CLOCK);
+    let clocks = clocks.enable_ext_hfosc();
 
     let p1 = nrf52840_hal::gpio::p1::Parts::new(peripherals.P1);
     let pins = nrf52840_hal::twim::Pins {
@@ -121,17 +140,50 @@ fn main() -> ! {
 
     let mut timer = nrf52840_hal::timer::Timer::new(peripherals.TIMER0);
 
-    let mut neotrellis = NeoTrellis::new(twim, &mut timer, None).unwrap();
+    let usb_periph = nrf52840_hal::usbd::UsbPeripheral::new(peripherals.USBD, &clocks);
+    let usb_bus = nrf52840_hal::usbd::Usbd::new(usb_periph);
+    let usb_bus_allocator: UsbBusAllocator<Usbd<UsbPeripheral>> = usb_bus;
+
+    let mut hid_device = HIDClass::new(&usb_bus_allocator, MouseReport::desc(), 60);
+    let mut usb_device = UsbDeviceBuilder::new(&usb_bus_allocator, UsbVidPid(0x5824, 0x27dd))
+        .manufacturer("AllThingsEmbedded")
+        .product("MyCool product")
+        .serial_number("TEST")
+        .device_class(0xef)
+        .build();
+
+    let mut neotrellis = NeoTrellis::new(twim, &mut timer, Some(0x2f)).unwrap();
     let mut pixels = neotrellis.neopixels();
 
     init_pixels(&mut pixels).unwrap();
 
-    let mut breathing_lights = BreathingLights::new(pixels);
+    let mut _breathing_lights = BreathingLights::new(pixels);
 
     defmt::info!("App started!");
 
+    let mut timer = timer.into_periodic();
+    timer.start(100_000u32);
+    let mut state = 4i8;
+    let mut count = 0u8;
     loop {
-        breathing_lights.show_next().unwrap();
+        // breathing_lights.show_next().unwrap();
+        usb_device.poll(&mut [&mut hid_device]);
+        if timer.wait() == Ok(()) {
+            if count < 10 {
+                count += 1;
+                continue;
+            }
+            let report = MouseReport {
+                x: 0,
+                y: state,
+                buttons: 0,
+                wheel: 0,
+            };
+            defmt::info!("Pushing report {}", state);
+            hid_device.push_input(&report).unwrap_or(0);
+            defmt::info!("Pushed report");
+            state = -state;
+        }
+        nb::block!(timer.wait()).unwrap();
     }
-    // cortex_m::asm::bkpt();
 }
