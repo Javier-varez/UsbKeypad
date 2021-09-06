7ff686445e377e06253cb06153778e57b2a7206d
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-09-06 23:21:17 +0200

    Use rtic 0.6 and usb stack

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ use core::sync::atomic::{AtomicUsize, Ordering};
 use defmt_rtt as _;
 use panic_probe as _;
 
-use adafruit_neotrellis::NeoTrellis;
 use embedded_graphics::{
     draw_target::DrawTarget,
     mono_font::{ascii::FONT_5X8, MonoTextStyle},
@@ -17,13 +16,10 @@ use embedded_graphics::{
     prelude::*,
     text::{Baseline, Text, TextStyleBuilder},
 };
-use nrf52840_hal::{self as _, gpio, pac, timer, twim};
-use shared_bus::BusManagerSimple;
+use nrf52840_hal as _;
 use tinybmp::Bmp;
 
 use display::NeoTrellisDisplay;
-
-use rtic::cyccnt::U32Ext as _;
 
 #[defmt::panic_handler]
 fn panic() -> ! {
@@ -105,27 +101,66 @@ where
     }
 }
 
-static HEART_DATA: &[u8; 246] = include_bytes!("../heart.bmp");
+#[rtic::app(device = nrf52840_hal::pac, peripherals = true, dispatchers = [USBD, QSPI, NFCT])]
+mod app {
+    use crate::scroll_text;
+    use adafruit_neotrellis::NeoTrellis;
+    use embedded_graphics::pixelcolor::Rgb888;
+    use nrf52840_hal::{self as _, gpio, pac, timer, twim};
+    use shared_bus::BusManagerSimple;
+    use tinybmp::Bmp;
 
-#[rtic::app(device = nrf52840_hal::pac, peripherals = true, monotonic = rtic::cyccnt::CYCCNT)]
-const APP: () = {
-    struct Resources {
+    use crate::apply_breathing_effect;
+    use crate::display::NeoTrellisDisplay;
+
+    use nrf52840_hal::clocks;
+    use nrf52840_hal::usbd;
+
+    use usb_device::{
+        bus::UsbBusAllocator,
+        device::{UsbDevice, UsbDeviceBuilder},
+        prelude::*,
+    };
+    use usbd_hid::descriptor::{MouseReport, SerializedDescriptor};
+    use usbd_hid::hid_class::HIDClass;
+
+    use dwt_systick_monotonic::DwtSystick;
+    use rtic::time::duration::Milliseconds;
+
+    const MONO_HZ: u32 = 64_000_000; // 64 MHz
+
+    #[monotonic(binds = SysTick, default = true, priority = 8)]
+    type MyMono = DwtSystick<MONO_HZ>;
+
+    #[local]
+    struct Local {
         timer: timer::Timer<pac::TIMER0>,
         i2c: BusManagerSimple<twim::Twim<pac::TWIM0>>,
         heart_bmp: Bmp<'static, Rgb888>,
+        usb_device: UsbDevice<'static, usbd::Usbd<usbd::UsbPeripheral<'static>>>,
     }
 
-    #[init(schedule = [run_display])]
-    fn init(mut cx: init::Context) -> init::LateResources {
-        // Initialize (enable) the monotonic timer (CYCCNT)
-        cx.core.DCB.enable_trace();
-        // required on Cortex-M7 devices that software lock the DWT (e.g. STM32F7)
-        cortex_m::peripheral::DWT::unlock();
-        cx.core.DWT.enable_cycle_counter();
+    #[shared]
+    struct Shared {
+        hid_class: HIDClass<'static, usbd::Usbd<usbd::UsbPeripheral<'static>>>,
+    }
 
-        let now = cx.start; // the start time of the system
+    #[init(
+        local = [
+            clocks: Option<clocks::Clocks<clocks::ExternalOscillator, clocks::Internal, clocks::LfOscStopped>> = None,
+            usb_buf_alloc: Option<UsbBusAllocator<usbd::Usbd<usbd::UsbPeripheral<'static>>>> = None,
+        ]
+    )]
+    fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
+        static HEART_DATA: &[u8; 246] = include_bytes!("../heart.bmp");
 
         let peripherals = cx.device;
+
+        let mut dcb = cx.core.DCB;
+        let dwt = cx.core.DWT;
+        let systick = cx.core.SYST;
+
+        let mono = DwtSystick::new(&mut dcb, dwt, systick, 64_000_000);
 
         let p1 = gpio::p1::Parts::new(peripherals.P1);
         let pins = twim::Pins {
@@ -140,24 +175,68 @@ const APP: () = {
 
         let heart_bmp = Bmp::<Rgb888>::from_slice(HEART_DATA).unwrap();
 
-        defmt::info!("App started!");
+        let clocks = clocks::Clocks::new(peripherals.CLOCK);
+        *cx.local.clocks = Some(clocks.enable_ext_hfosc());
 
-        cx.schedule
-            .run_display(now + 8_000_000u32.cycles())
-            .unwrap();
+        // Usb class
+        let usb_periph =
+            usbd::UsbPeripheral::new(peripherals.USBD, cx.local.clocks.as_ref().unwrap());
+        let usb_bus = usbd::Usbd::new(usb_periph);
+        *cx.local.usb_buf_alloc = Some(usb_bus.into());
+        let usb_bus_allocator = cx.local.usb_buf_alloc.as_ref().unwrap();
 
-        init::LateResources {
-            timer,
-            i2c,
-            heart_bmp,
-        }
+        let mut hid_class = HIDClass::new(usb_bus_allocator, MouseReport::desc(), 60);
+        let mut usb_device = UsbDeviceBuilder::new(usb_bus_allocator, UsbVidPid(0x5824, 0x27dd))
+            .manufacturer("AllThingsEmbedded")
+            .product("USB mouse")
+            .serial_number("00000000")
+            .device_class(0xef)
+            .build();
+        usb_device.poll(&mut [&mut hid_class]);
+
+        usb_task::spawn().unwrap();
+        run_display::spawn().unwrap();
+
+        (
+            Shared { hid_class },
+            Local {
+                timer,
+                i2c,
+                heart_bmp,
+                usb_device,
+            },
+            init::Monotonics(mono),
+        )
     }
 
-    #[task(resources = [i2c, timer, heart_bmp])]
+    #[task(local = [usb_device], shared = [hid_class], priority = 3)]
+    fn usb_task(mut cx: usb_task::Context) {
+        let usb_dev = cx.local.usb_device;
+        cx.shared.hid_class.lock(|hid| {
+            usb_dev.poll(&mut [hid]);
+        });
+        usb_task::spawn_after(Milliseconds(2u32)).unwrap();
+    }
+
+    #[task(shared = [hid_class], priority = 2)]
+    fn hid_task(mut cx: hid_task::Context) {
+        defmt::info!("moving mouse");
+        cx.shared.hid_class.lock(|hid| {
+            let report = MouseReport {
+                x: 0,
+                y: 10,
+                buttons: 0,
+                wheel: 0,
+            };
+            hid.push_input(&report).unwrap();
+        });
+    }
+
+    #[task(local = [i2c, timer, heart_bmp], priority = 1)]
     fn run_display(cx: run_display::Context) {
-        let timer = cx.resources.timer;
-        let i2c = cx.resources.i2c;
-        let heart_bmp = cx.resources.heart_bmp;
+        let timer = cx.local.timer;
+        let i2c = cx.local.i2c;
+        let heart_bmp = cx.local.heart_bmp;
 
         let neotrellis_devs = [
             NeoTrellis::new(i2c.acquire_i2c(), timer, Some(0x2E)).unwrap(),
@@ -168,15 +247,13 @@ const APP: () = {
         let mut display = NeoTrellisDisplay::new(neotrellis_devs);
         display.init().unwrap();
 
-        loop {
-            apply_breathing_effect(&mut display, timer, heart_bmp, 1000);
-            scroll_text(&mut display, timer, "Hi There!!");
+        // TODO(javier): Chunk these operations so that they keypad can be used concurrently
+        apply_breathing_effect(&mut display, timer, heart_bmp, 1000);
+        scroll_text(&mut display, timer, "Hi There!!");
 
-            defmt::info!("run_display finished");
-        }
-    }
+        defmt::info!("run_display finished");
 
-    extern "C" {
-        fn QSPI();
+        hid_task::spawn().unwrap();
+        run_display::spawn_after(Milliseconds(10u32)).ok();
     }
-};
+}
